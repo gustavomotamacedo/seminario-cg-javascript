4c69eebb60be1e33b58654fed86b431577e8df87
gustavomotamacedo <mota.macedo05@gmail.com>
2024-08-23 16:53:33 -0300

    feat: criando quadrado centralizado com canvas

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,16 @@
 let canvas1 = document.querySelector("#canvas1");
+let canvasCtx = canvas1.getContext("2d");
 
+let centerX = canvas1.getAttribute("width");
+let centerY = canvas1.getAttribute("height");
+centerX = centerX / 2;
+centerY = centerY / 2; 
+
+let rectW = 100;
+let rectH = 100;
+
+centerX = centerX - (rectW/2);
+centerY = centerY - (rectH/2);
+
+canvasCtx.fillStyle = "#00f"
+canvasCtx.fillRect(centerX, centerY, rectW, rectH);
